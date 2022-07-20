aeb577693df76b122d61aebb8f3b4ad9a9b630a7
DESKTOP-JIMMY\Jimmy <jimmy.lee@jeju-semi.com>
2022-07-20 20:01:28 +0900

    new files are updatedgit add .!

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+// var appHeader = {
+//     template: '<div>header</div>',
+//     methods: {
+//         addNum: function() {
+
+//         }
+//     }
+// }
+// <template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+// javascript - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+/* css */
+</style>
